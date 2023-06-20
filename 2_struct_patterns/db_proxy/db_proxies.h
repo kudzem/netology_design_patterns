07d4f40feb8c4160c09adc9a74791b382cb5881c
Alexander Kokrov <kudzem@mail.ru>
2023-06-20 15:55:58 +0200

    DB Proxy Demo

---
+++ 2_struct_patterns/db_proxy/db_proxies.h
@@ -0,0 +1,57 @@
+#pragma once
+
+class VeryHeavyDatabase {
+public:
+    std::string GetData(const std::string& key) const noexcept {
+        return "Very Big Data String: " + key;
+    }
+};
+
+class OneShotDB : VeryHeavyDatabase {
+
+public:
+    explicit OneShotDB(VeryHeavyDatabase* real_object, size_t shots = 1) : real_db_(real_object), _request_counter(shots) {}
+    std::string GetData(const std::string& key) noexcept {
+        if (_request_counter--) {
+            std::cout << "Get from real object\n";
+            return real_db_->GetData(key);
+        }
+        else {
+            std::cout << "Object is expired\n";
+        }
+        return "error";
+    }
+
+private:
+    size_t _request_counter;
+    VeryHeavyDatabase* real_db_;
+};
+
+class CacheProxyDB : VeryHeavyDatabase {
+public:
+    explicit CacheProxyDB(VeryHeavyDatabase* real_object) : real_db_(real_object) {}
+    std::string GetData(const std::string& key) noexcept {
+        if (cache_.find(key) == cache_.end()) {
+            std::cout << "Get from real object\n";
+            cache_[key] = real_db_->GetData(key);
+        }
+        else {
+            std::cout << "Get from cache\n";
+        }
+        return cache_.at(key);
+    }
+
+private:
+    std::map<std::string, std::string> cache_;
+    VeryHeavyDatabase* real_db_;
+};
+
+class TestDB : VeryHeavyDatabase {
+public:
+    explicit TestDB(VeryHeavyDatabase* real_object) : real_db_(real_object) {}
+    std::string GetData(const std::string& key) noexcept {
+        return "test_data\n";
+    }
+private:
+    VeryHeavyDatabase* real_db_;
+};
---
+++ 2_struct_patterns/db_proxy/proxy_demo.cpp
@@ -0,0 +1,21 @@
+#include <map>
+#include <string>
+#include <iostream>
+
+#include "db_proxies.h"
+
+int main() {
+    auto real_db = VeryHeavyDatabase();
+    auto cached_db = CacheProxyDB(std::addressof(real_db));
+    auto test_db = TestDB(std::addressof(real_db));
+    std::cout << cached_db.GetData("key") << std::endl;
+    std::cout << cached_db.GetData("key") << std::endl;
+    std::cout << test_db.GetData("key") << std::endl;
+
+    auto short_life_db = OneShotDB(std::addressof(real_db), 2);
+    std::cout << short_life_db.GetData("key") << std::endl;
+    std::cout << short_life_db.GetData("key") << std::endl;
+    std::cout << short_life_db.GetData("key") << std::endl;
+
+    return 0;
+}
