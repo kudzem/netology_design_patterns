355747f05397b71c1c40436d65653c963fcdb2c4
Alexander Kokrov <kudzem@mail.ru>
2023-06-29 21:44:18 +0200

    SOLID Principle fixes
    S - класс Data хранит в себе данные и их тип, а еще отвечает за их форматирование
      а нужно либо хранить, либо форматировать
    
    O - добавление нового типа данных приводит к изменению объъекта Data, SaveTo
    
    D - функция saveTo жестко зависит от интерфейса ofstream, что приводит к необходимости дублировать код илбо менять функцию
        при добавлении возможности форматированного печати
    	Нужно абстрагироваться от ofstream

--- 4_dry_solid/1_fix_solid_principle_mistakes/main.cpp
+++ 4_dry_solid/1_fix_solid_principle_mistakes/main.cpp
@@ -1,7 +1,15 @@
 #include "solid_example.h"
-
+#include <iostream>
 
 int main() 
 {
-    
+    FormatterJSON f1;
+    FormatterHTML f2;
+
+    auto text_to_print = "hello, solid principles!";
+
+    std::ofstream log("log.txt");
+
+    formatAndPrint(log, text_to_print, f1);
+    formatAndPrint(std::cout, text_to_print, f2);
 }
